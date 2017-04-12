11d84fd42fb92bbbf95a67431d4e12ae7961c46f
Greg-R <soapy-smith@comcast.net>
2017-04-11 20:00:06 -0400

    Changed simulate to false.

--- chapter7-implementation/part1-2-direct-gateway/wot-server.js
+++ chapter7-implementation/part1-2-direct-gateway/wot-server.js
@@ -10,13 +10,13 @@ var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-pirPlugin.start({'simulate': true, 'frequency': 2000}); //#B
-ledsPlugin.start({'simulate': true, 'frequency': 10000}); //#B
-dhtPlugin.start({'simulate': true, 'frequency': 10000}); //#B
+pirPlugin.start({'simulate': false, 'frequency': 2000}); //#B
+ledsPlugin.start({'simulate': false, 'frequency': 10000}); //#B
+dhtPlugin.start({'simulate': false, 'frequency': 10000}); //#B
 
 // External Plugins
-var coapPlugin = require('./plugins/external/coapPlugin');
-coapPlugin.start({'simulate': false, 'frequency': 10000});
+//var coapPlugin = require('./plugins/external/coapPlugin');
+//coapPlugin.start({'simulate': false, 'frequency': 10000});
 
 // HTTP Server
 var server = httpServer.listen(resources.pi.port, function () {
