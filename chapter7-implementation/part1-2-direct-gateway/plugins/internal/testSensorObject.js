171f67853fd2f4b73e724200c113ed8b56587051
Greg-R <soapy-smith@comcast.net>
2017-04-15 16:52:19 -0400

    Some cleanup.

--- chapter7-implementation/part1-2-direct-gateway/plugins/internal/testSensorObject.js
+++ chapter7-implementation/part1-2-direct-gateway/plugins/internal/testSensorObject.js
@@ -1,20 +1,28 @@
 /*jshint esversion: 6 */
 
+//  Basic tests of the sensor object.
+
 var resources = require('./../../resources/model');
 let TempHumSensor = require('./DHT22SensorObject');
 
-let params = {'simulate': false, 'frequency': 1000};
+let params = {
+    'simulate': false,
+    'frequency': 1000
+};
 
 let temphumsensor = new TempHumSensor(params);
 
 console.log(temphumsensor.model.temperature.value);
 
-temphumsensor.on('tempEvent', () => {console.log('tempEvent fired!');});
-temphumsensor.on('humidityEvent', () => {console.log('humidityEvent fired!');});
+temphumsensor.on('tempEvent', () => {
+    console.log('tempEvent fired!');
+});
+temphumsensor.on('humidityEvent', () => {
+    console.log('humidityEvent fired!');
+});
 
 temphumsensor.start();
 
 console.log(temphumsensor.model.temperature.value);
 
 console.log(temphumsensor.model);
-
