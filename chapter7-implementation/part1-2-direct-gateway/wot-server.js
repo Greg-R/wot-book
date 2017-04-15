ace09d3aca92cf31ba28afca95aff68dbaa3ae8d
Greg-R <soapy-smith@comcast.net>
2017-04-15 14:04:17 -0400

    Updated all to objects.

--- chapter7-implementation/part1-2-direct-gateway/wot-server.js
+++ chapter7-implementation/part1-2-direct-gateway/wot-server.js
@@ -1,18 +1,18 @@
 // Final version
 var httpServer = require('./servers/http'),
   wsServer = require('./servers/websockets'),
-  resources = require('./resources/model').resourcesObject;
+  resources = require('./resources/model');
 
 // Internal Plugins
-var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
-  pirPlugin = require('./plugins/internal/pirPlugin'), //#A
-  dhtPlugin = require('./plugins/internal/DHT22SensorPlugin'); //#A
+//var ledsPlugin = require('./plugins/internal/ledsPlugin'), //#A
+//  pirPlugin = require('./plugins/internal/pirPlugin'), //#A
+//  dhtPlugin = require('./plugins/internal/DHT22SensorPlugin'); //#A
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-pirPlugin.start({'simulate': false, 'frequency': 2000}); //#B
-ledsPlugin.start({'simulate': false, 'frequency': 10000}); //#B
-dhtPlugin.start({'simulate': false, 'frequency': 10000}); //#B
+//pirPlugin.start({'simulate': false, 'frequency': 2000}); //#B
+//ledsPlugin.start({'simulate': false, 'frequency': 10000}); //#B
+//dhtPlugin.start({'simulate': false, 'frequency': 10000}); //#B
 
 // External Plugins
 //var coapPlugin = require('./plugins/external/coapPlugin');
